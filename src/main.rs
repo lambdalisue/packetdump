e400a1f6bdb4df0998c4dc36e0713cdbd8da15b8
lambdalisue <lambdalisue@hashnote.net>
2020-01-03 01:08:57 +0900

    Add simple packet sniffing feature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::SystemTime;
+
+use pnet::datalink;
+use pnet::datalink::Channel::Ethernet;
+use pnet::datalink::NetworkInterface;
+use pnet::packet::ethernet::EthernetPacket;
+
+fn handle_ethernet_frame(
+    interface: &NetworkInterface,
+    time: &SystemTime,
+    ethernet: &EthernetPacket,
+) {
+    println!("{}: {:?}: {:?}", interface.name, time, ethernet,);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = mpsc::channel();
+    let mut handles: Vec<_> = datalink::interfaces()
+        .into_iter()
+        .filter(|iface| !iface.mac_address().is_zero())
+        .map(|iface| {
+            let tx = tx.clone();
+            thread::spawn(move || {
+                let mut rx = match datalink::channel(&iface, Default::default()) {
+                    Ok(Ethernet(_, rx)) => rx,
+                    Ok(_) => panic!("unhandled channel type"),
+                    Err(e) => panic!("An error occured when creating the datalink channel: {}", e),
+                };
+                loop {
+                    match rx.next() {
+                        Ok(packet) => tx
+                            .send((iface.clone(), SystemTime::now(), packet.to_owned()))
+                            .unwrap(),
+                        Err(_) => continue,
+                    }
+                }
+            })
+        })
+        .collect();
+    handles.push(thread::spawn(move || loop {
+        loop {
+            match rx.recv() {
+                Ok((iface, time, packet)) => match EthernetPacket::new(&packet) {
+                    Some(packet) => handle_ethernet_frame(&iface, &time, &packet),
+                    _ => continue,
+                },
+                Err(_) => continue,
+            }
+        }
+    }));
+
+    for h in handles {
+        h.join().unwrap();
+    }
 }
